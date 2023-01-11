b5ebb7b18f4c5108dbded5af23d759fb7726d0d5
homile <ssanho2@naver.com>
2023-01-11 21:17:42 +0900

    fix: 검색어 없을 경우 메시지 추가

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -8,15 +8,17 @@ interface dataType {
 
 const SearchBar = () => {
   const [search, setSearch] = useState('');
-  const [data, setData] = useState<dataType[]>();
+  const [data, setData] = useState<dataType[] | null>(null);
   const [select, setSelect] = useState(-1);
 
+  // 데이터 가져오기
   const dataGet = () => {
     getData(search).then(res => {
       setData(res.data);
     });
   };
 
+  // up, down에 따른 요소 추천 검색어 포커싱
   const handleKeyUp = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (data) {
       if (event.code === 'ArrowUp' && select >= 1) {
@@ -29,12 +31,13 @@ const SearchBar = () => {
   };
 
   useEffect(() => {
+    // 디바운스 처리 1초
     const debounce = setTimeout(() => {
       if (search) {
         return dataGet();
       }
       if (search === '') {
-        setData([]);
+        setData(null);
       }
     }, 1000); //->setTimeout 설정
     return () => clearTimeout(debounce); //->clearTimeout 바로 타이머 제거
@@ -50,17 +53,19 @@ const SearchBar = () => {
         className="p-5 m-10 w-300px h-16 text-12 border-2 rounded-5"
         onChange={e => setSearch(e.target.value)}
       />
-      {data ? (
-        <ul>
-          {data.map((sick, idx) => {
+      <ul>
+        {data ? (
+          data.map((sick, idx) => {
             return (
               <li key={sick.sickCd} className={`${idx === select ? 'text-red-400' : ''}`}>
                 {sick.sickNm}
               </li>
             );
-          })}
-        </ul>
-      ) : null}
+          })
+        ) : (
+          <li>검색어 없음</li>
+        )}
+      </ul>
     </div>
   );
 };
